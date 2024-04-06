9c3a272dc882857802dbf7e16391e74083d64296
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-04-06 02:36:36 -1200

    last

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,168 @@
+'use strict';
 
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * Mobile navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+});
+
+addEventOnElements(navLinks, "click", function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("nav-active");
+});
+
+
+
+/**
+ * Header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
+
+
+
+/**
+ * Element tilt effect
+ */
+
+const tiltElements = document.querySelectorAll("[data-tilt]");
+
+const initTilt = function (event) {
+
+  /** get tilt element center position */
+  const centerX = this.offsetWidth / 2;
+  const centerY = this.offsetHeight / 2;
+
+  const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
+  const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
+
+  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${tiltPosY - (tiltPosY * 2)}deg)`;
+
+}
+
+addEventOnElements(tiltElements, "mousemove", initTilt);
+
+addEventOnElements(tiltElements, "mouseout", function () {
+  this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+});
+
+
+
+/**
+ * Tab content
+ */
+
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+let lastActiveTabBtn = tabBtns[0];
+let lastActiveTabContent = tabContents[0];
+
+const filterContent = function () {
+
+  if (!(lastActiveTabBtn === this)) {
+
+    lastActiveTabBtn.classList.remove("active");
+    lastActiveTabContent.classList.remove("active");
+
+    this.classList.add("active");
+    lastActiveTabBtn = this;
+
+    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+    currentTabContent.classList.add("active");
+    lastActiveTabContent = currentTabContent;
+
+  }
+
+}
+
+addEventOnElements(tabBtns, "click", filterContent);
+
+
+
+/**
+ * Custom cursor
+ */
+
+const cursors = document.querySelectorAll("[data-cursor]");
+const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
+
+window.addEventListener("mousemove", function (event) {
+
+  const posX = event.clientX;
+  const posY = event.clientY;
+
+  /** cursor dot position */
+  cursors[0].style.left = `${posX}px`;
+  cursors[0].style.top = `${posY}px`;
+
+  /** cursor outline position */
+  setTimeout(function () {
+    cursors[1].style.left = `${posX}px`;
+    cursors[1].style.top = `${posY}px`;
+  }, 80);
+
+});
+
+/** add hovered class when mouseover on hoverElements */
+addEventOnElements(hoveredElements, "mouseover", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.add("hovered");
+  }
+});
+
+/** remove hovered class when mouseout on hoverElements */
+addEventOnElements(hoveredElements, "mouseout", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.remove("hovered");
+  }
+});
 
 const translations = {
   ar: {
-    home: "الصفحة الرئيسية",
+    home: " الرئيسية",
     
     arabic: "AR",
     english: "EN",
@@ -65,7 +225,10 @@ const translations = {
     55:"اشتري الآن",
     56:"العنوان",
     57:"من خلال الاشتراك في قائمتنا البريدية، سوف تكون دائمًا مطلعًا على آخر الأخبار منا.",
-   
+    58:"نحن نحترم خصوصيتك",
+    59:"ارسل طلب بمحتويات اي تطبيق او نظام  الي تريدها وارفق الايميل مع الطلب  للتواصل",
+    60:"الادوات",
+    61:"التواصل",
   },
     en: {
       home: "Home",
@@ -130,6 +293,10 @@ const translations = {
     55:"Buy Now",
     56:"Address",
     57:"By subscribing to our mailing list you will always be update with the latest news from us.",
+    58:"We respect your privacy",
+    59:"Send a request with the contents of any application or system you want and attach the email to the request for communication",
+    60:"Tools",
+    61:"Contact Us",
      
 
     },
